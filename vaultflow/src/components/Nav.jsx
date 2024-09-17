5a08adb4e5e52bcc4a894601d20f04d351459937
Diesos <ozturk@live.Fr>
2024-09-17 11:07:37 +0200

    style: Update font and styling in Nav component

--- vaultflow/src/components/Nav.jsx
+++ vaultflow/src/components/Nav.jsx
@@ -3,23 +3,62 @@ import logo from "../assets/logo.svg";
 
 export const Nav = () => {
   const Navbar = styled.nav`
-    background-color: black;
-    opacity: 50%;
+    background-color: rgba(0, 0, 0, 0.5);
     height: 72px;
     border-radius: 25px;
     display: flex;
     justify-content: space-between;
     padding: 20px;
     width: 95%;
+    margin-top: 30px;
+  `;
+
+  const Paragraph = styled.p`
+    color: white;
+  `;
+
+  const BoldParagraph = styled(Paragraph)`
+    font-weight: 700;
+    font-size: 22px;
+  `;
+
+  const DivLeft = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  `;
+
+  const DivMid = styled.ul`
+    display: flex;
+    justify-content: center;
+    gap: 32px;
+    list-style: none;
+    font-weight: 400;
+    color: white;
+    align-items: center;
+    font-size: 16px;
+  `;
+
+  const DivRight = styled.button`
+    border: none;
+    border-radius: 50px;
+    padding: 10px 20px;
+    align-self: center;
+    background-color: #ececec;
   `;
   return (
-    <div style={{ alignItems: "center" }}>
+    <div style={{ display: "flex", justifyContent: "center" }}>
       <Navbar>
-        <div style={{ display: "flex", alignItems: "center", gap: "5px" }}>
-          <img src={logo} style={{ height: "2em" }} alt="Logo"></img>
-          <p style={{ color: "white" }}>Vault Flow</p>
-        </div>
-        <p style={{ color: "white" }}>Hello</p>
+        <DivLeft>
+          <img src={logo} alt="Logo"></img>
+          <BoldParagraph>Vaultflow</BoldParagraph>
+        </DivLeft>
+        <DivMid>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>About us</li>
+        </DivMid>
+        <DivRight>Download the app</DivRight>
       </Navbar>
     </div>
   );
