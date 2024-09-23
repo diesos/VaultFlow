c7a4c219fece6b4b566c0124d469a639e2836005
Diesos <ozturk@live.Fr>
2024-09-23 22:43:04 +0200

    chore: Add Features component with styled paragraph

---
+++ vaultflow/src/components/Features.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledParagraph = styled.p`
+  font-size: 20px;
+`;
+export const Features = () => {
+  return (
+    <>
+      <section>
+        <StyledParagraph>
+          Trusted by teams at over 1,000 of the world’s leading organizations
+        </StyledParagraph>
+      </section>
+    </>
+  );
+};
